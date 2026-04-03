16c6be79bec2240dfd82fbac039091c63fcef4b3
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-03 16:43:36 +0200

    zsh: Shorten and organize aliases.zsh

--- zsh/conf.d/aliases.zsh
+++ zsh/conf.d/aliases.zsh
@@ -1,31 +1,37 @@
-if [ -x "$(command -v xclip)" ]; then
-	alias -g C='| xclip -in -selection clipboard'
-elif [ -x "$(command -v pbcopy)" ]; then
-	alias -g C='| pbcopy'
-fi
+function s { [ $# = 0 ] && sudo -s || sudo "$@" }
 
-if [ -x "$(command -v photoflare)" ]; then
-    alias paint="photoflare &>/dev/null"
-elif [ -x "$(command -v pinta)" ]; then
-    alias paint=pinta
-fi
+# Aliases
 
-if [ -x "$(command -v rg)" ]; then
+if command -v rg >/dev/null; then
 	alias ack=rg
-elif [ -x "$(command -v ag)" ]; then
+elif command -v ag >/dev/null; then
 	alias ack=ag
 fi
 
-if [ -x "$(command -v jq)" ]; then
-	alias jqresults="jq '.. | objects | select(has(\"results\") and (.results | length > 0)) | .results'"
+command -v bazel >/dev/null   && alias bzllock="bazel mod deps --lockfile_mode=update"
+command -v eza >/dev/null     && alias tree="eza -T"
+command -v jq >/dev/null      && alias jqresults="jq '.. | objects | select(has(\"results\") and (.results | length > 0)) | .results'"
+command -v libtree >/dev/null && alias ldt=libtree
+
+if command -v xdg-open >/dev/null; then
+	alias open="xdg-open"
+elif command -v gnome-open >/dev/null; then
+	alias open="gnome-open"
 fi
 
-if [ -x "$(command -v eza)" ]; then
-	alias tree="eza -T"
+if command -v photoflare >/dev/null; then
+	alias paint="photoflare &>/dev/null"
+elif command -v pinta >/dev/null; then
+	alias paint=pinta
 fi
 
-test -x "$(command -v libtree)" && alias ldt=libtree
-test -x "$(command -v bazel)" && alias bzllock="bazel mod deps --lockfile_mode=update"
+# Global Aliases
+
+if command -v xclip >/dev/null; then
+	alias -g C='| xclip -in -selection clipboard'
+elif command -v pbcopy >/dev/null; then
+	alias -g C='| pbcopy'
+fi
 
 alias -g D='| dot -Tpdf'
 alias -g G='| grep'
@@ -35,12 +41,3 @@ alias -g N='&> /dev/null'
 alias -g P='| open -fa Preview'
 alias -g T='| tail'
 alias -g XB="':(exclude)MODULE.bazel.lock'"
-
-if [ -x "$(command -v xdg-open)" ]; then
-	alias open="xdg-open"
-else
-	[ -x "$(command -v gnome-open)" ] &&
-	alias open="gnome-open"
-fi
-
-function s { [ $# = 0 ] && sudo -s || sudo "$@" }
