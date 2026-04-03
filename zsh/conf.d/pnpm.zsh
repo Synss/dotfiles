94fb933a439b55662e1440e4874be809573219ad
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-03 15:23:35 +0200

    zsh: Improve organization
    `.zshrc` is hidden and hard to search.  Move the content
    to `conf.d`.
    
    It's better organized like this anyway.

---
+++ zsh/conf.d/pnpm.zsh
@@ -0,0 +1,8 @@
+# vim: syn=zsh
+[ -d "$HOME/.local/share/pnpm" ] || return 0
+
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
