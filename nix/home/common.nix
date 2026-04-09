d5c6f236a5bd7c548431272ac19cbc01c26f5288
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-09 22:14:31 +0200

    nix: Port git config to programs.git and programs.delta

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -27,7 +27,6 @@
         eza
         fd
         fzf
-        git
         jq
         just
         yq-go
@@ -50,6 +49,53 @@
       ".zshenv".text = ''
         export ZDOTDIR="${dotfilesDir}/zsh"
       '';
+    };
+  };
+
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        alias = {
+          au = "add -u";
+          ap = "add -p";
+          amend = "commit --amend";
+          branch-name = "rev-parse --abbrev-ref HEAD";
+          cane = "commit --amend --no-edit";
+          ci = "commit";
+          co = "checkout";
+          fixup = "commit --fixup";
+          names = "diff --name-only";
+          retop = "!f() { branch=\${1:-master}; git fetch origin $branch:$branch && git rebase $branch; }; f";
+          ri = "rebase -i";
+          unstage = "reset HEAD --";
+        };
+        user = {
+          name = "Mathias Laurin";
+          email = "Mathias.Laurin+github.com@gmail.com";
+        };
+        commit.verbose = true;
+        merge.conflictStyle = "zdiff3";
+        pull.rebase = true;
+        push.default = "upstream";
+        rebase.autosquash = true;
+      };
+    };
+
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
+      options = {
+        navigate = true;
+        dark = {
+          dark = true;
+          syntax-theme = "gruvbox-dark";
+        };
+        light = {
+          light = true;
+          syntax-theme = "gruvbox-light";
+        };
+      };
     };
   };
 }
