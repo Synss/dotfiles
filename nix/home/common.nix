00cea62bfc1e4e92994824a6e880586897b07222
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-09 18:29:14 +0200

    nix: Add terminal packages to home.packages
    No python313/314, let's handle that with `uv python install ...`.

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -1,14 +1,41 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home = {
     username = builtins.getEnv "USER";
     homeDirectory = builtins.getEnv "HOME";
     stateVersion = "24.11";
 
-    packages = with pkgs; [
-      deadnix
-      nixfmt
-      statix
-    ];
+    packages =
+      with pkgs;
+      [
+        # nix tooling
+        deadnix
+        nixfmt
+        statix
+
+        # terminal tools
+        bat
+        bazelisk
+        delta
+        direnv
+        eza
+        fd
+        fzf
+        git
+        jq
+        just
+        yq-go
+        neovim
+        pnpm
+        ripgrep
+        shellcheck
+        tmux
+        uv
+        vivid
+        zoxide
+        zsh
+      ]
+      ++ lib.optionals stdenv.isLinux [ ]
+      ++ lib.optionals stdenv.isDarwin [ ];
   };
 }
