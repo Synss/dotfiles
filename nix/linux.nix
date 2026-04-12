974faad4bcc73201d269d99f5ee6f1133c5226a1
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-12 18:09:38 +0200

    nix: Configure alacritty

--- nix/linux.nix
+++ nix/linux.nix
@@ -13,5 +13,14 @@
   programs.alacritty = {
     enable = true;
     package = config.lib.nixGL.wrap pkgs.alacritty;
+    settings = {
+      window = {
+        padding = {
+          x = 1;
+          y = 1;
+        };
+        startup_mode = "Fullscreen";
+      };
+    };
   };
 }
