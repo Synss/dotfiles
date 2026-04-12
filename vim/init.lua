6e929f0cc24899d76ef9b266f374530e1744bf4a
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-12 14:52:52 +0200

    vim: Prefer `<leader>d` for diagnostic

--- vim/init.lua
+++ vim/init.lua
@@ -136,7 +136,7 @@ nnoremap("<leader>Q", "{gq}")
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
