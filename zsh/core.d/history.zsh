12d6fe36c55ad6c7b7c5745e0cf16ccae6ca3010
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-12 22:03:43 +0200

    zsh: Group keybindings to avoid conflicts

--- zsh/core.d/history.zsh
+++ zsh/core.d/history.zsh
@@ -7,6 +7,3 @@ HISTIGNORE="[bf]g:cd:df:exit:free:h:hh:l[al]:s:screen:which"
 [ -n "$UID" ] && [ -n "$GID" ] &&
 	touch "$HISTFILE" &&
 	chown "$UID:$GID" "$HISTFILE"
-
-bindkey "^P" history-incremental-search-backward
-bindkey "^N" history-incremental-search-forward
---
+++ zsh/core.d/keybindings.zsh
@@ -0,0 +1,7 @@
+# History navigation
+bindkey "^P" history-incremental-search-backward
+bindkey "^N" history-incremental-search-forward
+
+# zsh-autosuggestions: Ctrl-f accepts full suggestion
+# Alt-f / Esc+W step through word by word (vi-forward-word, default partial-accept)
+bindkey "^f" autosuggest-accept
