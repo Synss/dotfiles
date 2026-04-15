650a2ea79626538927463d416aed09270f28aafc
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-15 23:59:31 +0200

    nix: Configure alacritty for macos

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -1,3 +1,21 @@
 _: {
-  programs.alacritty.enable = true;
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        decorations = "Transparent";
+        padding = {
+          x = 1;
+          y = 1;
+        };
+        startup_mode = "Fullscreen";
+      };
+      terminal = {
+        shell = {
+          program = "zsh";
+          args = [ "-l" ];
+        };
+      };
+    };
+  };
 }
