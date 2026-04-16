8a5c7ff55867e183a5ae5f578e55a060aa83ba51
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-16 23:10:42 +0200

    zsh: make gruvbox themes for glow
     * gruvbox-light and -dark
     * set gruvbox-light as the default theme

--- zsh/conf.d/80-glow.zsh
+++ zsh/conf.d/80-glow.zsh
@@ -1,3 +1,14 @@
 command -v glow >/dev/null || return 0
 
-alias glow="glow --pager"
+__GLOW_STYLE_DIR="$(readlink -f "$(dirname "$0")/../../_config/glow/styles/")"
+
+# Pick light or dark themes
+if [[ -n "$COLORFGBG" ]] && [[ ${COLORFGBG##*;} -lt 8 ]]; then
+    __GLOW_STYLE="$__GLOW_STYLE_DIR/gruvbox-dark.json"
+else
+    __GLOW_STYLE="$__GLOW_STYLE_DIR/gruvbox-light.json"
+fi
+
+alias glow="glow --style \"$__GLOW_STYLE\" --pager"
+
+unset __GLOW_STYLE_DIR
