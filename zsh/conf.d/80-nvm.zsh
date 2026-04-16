ecaa7cf71a9e4ab94b731596555561cd6bdda572
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-16 22:01:34 +0200

    zsh: Uninstall nvm
    We have node in `nix` now, that should be sufficient.

--- zsh/conf.d/80-nvm.zsh
+++
@@ -1,6 +0,0 @@
-[ -d "$HOME/.nvm" ] || return 0
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-autoload -U +X bashcompinit && bashcompinit
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
