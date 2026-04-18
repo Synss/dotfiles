3823f8642cbd794e5db21856ec548da2f2f200dd
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-18 07:42:07 +0200

    nix: Remove theming from config
    now handled consistently by neovim

--- nix/home.nix
+++ nix/home.nix
@@ -77,17 +77,7 @@
     delta = {
       enable = true;
       enableGitIntegration = true;
-      options = {
-        navigate = true;
-        dark = {
-          dark = true;
-          syntax-theme = "gruvbox-dark";
-        };
-        light = {
-          light = true;
-          syntax-theme = "gruvbox-light";
-        };
-      };
+      options.navigate = true;
     };
 
     direnv = {
