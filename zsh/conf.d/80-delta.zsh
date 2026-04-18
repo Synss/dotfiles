b50c7816bdd7b7af765d594d6532de297cb123ba
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-18 23:22:31 +0200

    nix: Fixup theming def for `delta`

--- zsh/conf.d/80-delta.zsh
+++ zsh/conf.d/80-delta.zsh
@@ -2,8 +2,12 @@ command -v delta >/dev/null || return 0
 
 delta() {
   local theme="${DARK_THEME}"
+  local mode="--dark"
   if [[ -n "${NVIM}" && -f "${NVIM}.theme" ]]; then
-    [[ "$(< "${NVIM}.theme")" == "light" ]] && theme="${LIGHT_THEME}"
+    if [[ "$(< "${NVIM}.theme")" == "light" ]]; then
+      theme="${LIGHT_THEME}"
+      mode="--light"
+    fi
   fi
-  command delta --features="$theme" "$@"
+  command delta --syntax-theme="$theme" "$mode" "$@"
 }
