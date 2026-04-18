fcd7e3ba924092c52f2be4480a91db2bbf638d0a
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-18 10:19:11 +0200

    zsh: remove fzf-tab
    Added too many completion utils at once previously---usage is getting
    confusing.  Let's streamline.

--- zsh/conf.d/20-fzf-tab.zsh
+++
@@ -1,7 +0,0 @@
-command -v fzf >/dev/null || test -d "$ZDOTDIR/plugins/fzf-tab.plugins.zsh" || return 0
-
-source "$ZDOTDIR/plugins/fzf-tab.git/fzf-tab.plugin.zsh"
-
-# preview directory's content with eza when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-zstyle ':fzf-tab:*' use-fzf-default-opts yes
