a3941a9a93984dd9ff51a8d33da3e483b09d79d2
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-19 00:38:01 +0200

    zsh: document some more keybindings

--- zsh/conf.d/70-keybindings.zsh
+++ zsh/conf.d/70-keybindings.zsh
@@ -1,9 +1,14 @@
 bindkey -v
+KEYTIMEOUT=5	# 50ms to complete a key sequence; reduces Esc lag in vi mode
 
 # History navigation
 bindkey "^P" history-incremental-search-backward
 bindkey "^N" history-incremental-search-forward
+# ^R: fzf history search (replaces the built-in)
 
-# zsh-autosuggestions: Ctrl-f accepts full suggestion
-# Alt-f / Esc+W step through word by word (vi-forward-word, default partial-accept)
+# ^T: paste a file/directory path from fzf into the command line
+
+# zsh-autosuggestions:
+# ^F: accepts full suggestion
+# Esc+W: vi-forward-word
 bindkey "^f" autosuggest-accept
