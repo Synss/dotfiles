360fff5571188ee7b4b8493bd8329aa6daf6bafe
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-22 01:29:34 +0200

    vim: Tweaks for Oil
     * Exit with `q`
     * Show current path in lualine
     * Start relative to the file

--- vim/plugin/oil.lua
+++ vim/plugin/oil.lua
@@ -1,2 +1,22 @@
-require("oil").setup()
-vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
+local oil = require("oil")
+
+oil.setup({
+	keymaps = {
+		-- Navigation
+		["<C-H>"] = "<C-W><C-H>",
+		["<C-J>"] = "<C-W><C-J>",
+		["<C-K>"] = "<C-W><C-K>",
+		["<C-L>"] = "<C-W><C-L>",
+		--
+		["q"] = { "actions.close", mode = "n" },
+	},
+})
+
+vim.keymap.set("n", "-", function()
+	local buf_name = vim.api.nvim_buf_get_name(0)
+	if buf_name ~= "" and vim.bo.buftype == "" then
+		oil.open(vim.fs.dirname(buf_name))
+	else
+		oil.open()
+	end
+end, { desc = "Start Oil relative to the current file" })
