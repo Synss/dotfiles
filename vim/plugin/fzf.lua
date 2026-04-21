7523fc2acaa2018f23c3aede55870551737cf2a7
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-21 21:50:07 +0200

    vim: fzf.files not floating

--- vim/plugin/fzf.lua
+++ vim/plugin/fzf.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "<leader>fg", ":FzfLua live_grep<CR>", { desc = "Live grep" 
 require("fzf-lua").setup({
 	files = {
 		cmd = "fd --type f --hidden --exclude .git --exclude .gitmodules",
+		winopts = { split = "belowright new" },
 	},
 	grep = {
 		rg_opts = "--hidden --glob '!.git' --glob '!.gitmodules'",
