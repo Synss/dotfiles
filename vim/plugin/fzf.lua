043647ca902259a0080e6f0944bab9230ef0a0d4
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-22 01:01:00 +0200

    vim: prefer <cmd> to :

--- vim/plugin/fzf.lua
+++ vim/plugin/fzf.lua
@@ -1,7 +1,7 @@
-vim.keymap.set("n", "<leader>fb", ":FzfLua buffers<CR>", { desc = "Buffers" })
-vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>", { desc = "Find files" })
-vim.keymap.set("n", "<leader>fr", ":FzfLua oldfiles<CR>", { desc = "Recent files" })
-vim.keymap.set("n", "<leader>fg", ":FzfLua live_grep<CR>", { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", { desc = "Buffers" })
+vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua oldfiles<CR>", { desc = "Recent files" })
+vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", { desc = "Live grep" })
 
 require("fzf-lua").setup({
 	files = {
