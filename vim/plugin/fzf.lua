d7a63b75a9f3443171f33bc5fe2ed3014dfffcd8
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-22 17:24:10 +0200

    vim: Always open fzf-lua in the current window

--- vim/plugin/fzf.lua
+++ vim/plugin/fzf.lua
@@ -1,17 +1,21 @@
 local fzf = require("fzf-lua")
 
+local function in_current_win(picker)
+	return function()
+		picker({ winopts = { split = vim.api.nvim_get_current_win() } })
+	end
+end
+
 local function map_fzf_split(key, cmd, label)
 	vim.keymap.set("n", "<Leader>f" .. key, function()
 		fzf.files({ winopts = { split = cmd } })
 	end, { desc = "Find files " .. label })
 end
 
-vim.keymap.set("n", "<Leader>fb", "<Cmd>FzfLua buffers<CR>", { desc = "Buffers" })
-vim.keymap.set("n", "<Leader>ff", function()
-	fzf.files({ winopts = { split = vim.api.nvim_get_current_win() } })
-end, { desc = "Find files" })
-vim.keymap.set("n", "<Leader>fr", "<Cmd>FzfLua oldfiles<CR>", { desc = "Recent files" })
-vim.keymap.set("n", "<Leader>fg", "<Cmd>FzfLua live_grep<CR>", { desc = "Live grep" })
+vim.keymap.set("n", "<Leader>fb", in_current_win(fzf.buffers), { desc = "Buffers" })
+vim.keymap.set("n", "<Leader>ff", in_current_win(fzf.files), { desc = "Find files" })
+vim.keymap.set("n", "<Leader>fr", in_current_win(fzf.oldfiles), { desc = "Recent files" })
+vim.keymap.set("n", "<Leader>fg", in_current_win(fzf.live_grep), { desc = "Live grep" })
 
 map_fzf_split("h", "leftabove vnew", "left")
 map_fzf_split("j", "rightbelow new", "below")
