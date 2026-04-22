0fe246ca4ecf9d20209972ca46c5febc93f64dae
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-22 16:44:27 +0200

    vim: Add `<Leader>f{h..l}` commands consistent
    consistent with `<Leader>s{..}` and `<Leader>t{..}`

--- vim/plugin/fzf.lua
+++ vim/plugin/fzf.lua
@@ -1,12 +1,24 @@
+local fzf = require("fzf-lua")
+
+local function map_fzf_split(key, cmd, label)
+	vim.keymap.set("n", "<Leader>f" .. key, function()
+		fzf.files({ winopts = { split = cmd } })
+	end, { desc = "Find files " .. label })
+end
+
 vim.keymap.set("n", "<Leader>fb", "<Cmd>FzfLua buffers<CR>", { desc = "Buffers" })
-vim.keymap.set("n", "<Leader>ff", "<Cmd>FzfLua files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<Leader>ff", function()
+	fzf.files({ winopts = { split = vim.api.nvim_get_current_win() } })
+end, { desc = "Find files" })
 vim.keymap.set("n", "<Leader>fr", "<Cmd>FzfLua oldfiles<CR>", { desc = "Recent files" })
 vim.keymap.set("n", "<Leader>fg", "<Cmd>FzfLua live_grep<CR>", { desc = "Live grep" })
 
-require("fzf-lua").setup({
-	files = {
-		winopts = { split = "belowright new" },
-	},
+map_fzf_split("h", "leftabove vnew", "left")
+map_fzf_split("j", "rightbelow new", "below")
+map_fzf_split("k", "leftabove new", "above")
+map_fzf_split("l", "rightbelow vnew", "right")
+
+fzf.setup({
 	grep = {
 		rg_opts = "--hidden --glob '!.git' --glob '!.gitmodules'",
 	},
