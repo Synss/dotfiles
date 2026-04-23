145b05856699c3be65df52a44b2afabc4d38c345
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-23 22:41:56 +0200

    vim: Fixup fzf search file in new split keymaps

--- vim/plugin/fzf.lua
+++ vim/plugin/fzf.lua
@@ -1,4 +1,5 @@
 local fzf = require("fzf-lua")
+local fzf_actions = require("fzf-lua.actions")
 
 local function in_current_win(picker)
 	return function()
@@ -6,10 +7,24 @@ local function in_current_win(picker)
 	end
 end
 
-local function map_fzf_split(key, cmd, label)
-	vim.keymap.set("n", "<Leader>f" .. key, function()
-		fzf.files({ winopts = { split = cmd } })
-	end, { desc = "Find files " .. label })
+local function fzf_in_split(split_cmd)
+	return function()
+		vim.cmd(split_cmd)
+		local win = vim.api.nvim_get_current_win()
+		fzf.files({
+			winopts = { split = win },
+			actions = {
+				["default"] = function(selected, opts)
+					if selected and #selected > 0 then
+						vim.api.nvim_set_current_win(win)
+						fzf_actions.file_edit(selected, opts)
+					elseif vim.api.nvim_win_is_valid(win) then
+						vim.api.nvim_win_close(win, true)
+					end
+				end,
+			},
+		})
+	end
 end
 
 vim.keymap.set("n", "<Leader>fb", in_current_win(fzf.buffers), { desc = "Buffers" })
@@ -17,10 +32,10 @@ vim.keymap.set("n", "<Leader>ff", in_current_win(fzf.files), { desc = "Find file
 vim.keymap.set("n", "<Leader>fr", in_current_win(fzf.oldfiles), { desc = "Recent files" })
 vim.keymap.set("n", "<Leader>fg", in_current_win(fzf.live_grep), { desc = "Live grep" })
 
-map_fzf_split("h", "leftabove vnew", "left")
-map_fzf_split("j", "rightbelow new", "below")
-map_fzf_split("k", "leftabove new", "above")
-map_fzf_split("l", "rightbelow vnew", "right")
+vim.keymap.set("n", "<Leader>fh", fzf_in_split("leftabove vnew"), { desc = "Find files left" })
+vim.keymap.set("n", "<Leader>fj", fzf_in_split("rightbelow new"), { desc = "Find files below" })
+vim.keymap.set("n", "<Leader>fk", fzf_in_split("leftabove new"), { desc = "Find files above" })
+vim.keymap.set("n", "<Leader>fl", fzf_in_split("rightbelow vnew"), { desc = "Find files right" })
 
 fzf.setup({
 	grep = {
