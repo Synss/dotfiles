ab03b151ffe8fc05d4a2c987dd374c28eb54b0c4
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-24 09:56:48 +0200

    vim: Add keymap to open session management in fzf-lua

--- vim/plugin/auto-session.lua
+++ vim/plugin/auto-session.lua
@@ -1,4 +1,12 @@
 require("auto-session").setup({
 	purge_after_minutes = 43200, -- min, or 30 days
 	legacy_cmds = false,
+	session_lens = {
+		picker      = "fzf",
+		previewer   = "active_buffer", -- or "summary"
+		picker_opts = {
+			preview = { hidden = false },
+		},
+	},
 })
+vim.keymap.set("n", "<Leader>fs", "<Cmd>AutoSession search<CR>", { desc = "Search sessions" })
