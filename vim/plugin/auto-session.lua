c5788b0e29a17e57fc368d960e08240b39b400ed
Mathias Laurin <mathias.laurin+github.com@gmail.com>
2026-04-25 22:27:56 +0200

    vim: Fixup auto-session
    For `auto-session`, the CWD is part of the key. So, if it's changed
    during a session, the session will be saved under the new CWD and not
    the original one. This is not how I work, so let's change this behavior.

--- vim/plugin/auto-session.lua
+++ vim/plugin/auto-session.lua
@@ -1,6 +1,12 @@
+local startup_cwd = vim.fn.getcwd()
+
 require("auto-session").setup({
 	purge_after_minutes = 43200, -- min, or 30 days
 	legacy_cmds = false,
+	pre_save_cmds = {
+		-- Save session from the path nvim started
+		function() vim.cmd.cd(startup_cwd) end,
+	},
 	session_lens = {
 		picker      = "fzf",
 		previewer   = "active_buffer", -- or "summary"
