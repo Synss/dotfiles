35027919bb20c8eb61e6eae0a1626971b5811bc8
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2026-05-01 21:42:58 +0200

    vim: Add type annotations to `autoresize` plugin

--- vim/lua/autoresize/config.lua
+++ vim/lua/autoresize/config.lua
@@ -1,16 +1,22 @@
 local M = {}
 
+---@class autoresize.Config
+---@field ratio number Height ratio for the focused window (default: 1.25)
+---@field exclude_buftypes string[] Buffer types excluded from resizing
 M.defaults = {
 	ratio = 5 / 4,
 	exclude_buftypes = { "quickfix" },
 }
 
+---@type autoresize.Config
 M.options = vim.deepcopy(M.defaults)
 
+---@param opts? autoresize.Config
 function M.setup(opts)
 	M.options = vim.tbl_deep_extend("force", M.options, opts or {})
 end
 
+---@return autoresize.Config
 function M.get()
 	return M.options
 end
