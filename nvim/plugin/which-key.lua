87b36685466e0ebf939833f03174bc92343dd542
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2026-05-17 00:04:09 +0200

    vim: Set icons for `[*` and `]*` in which-key

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -28,4 +28,50 @@ wk.add({
 	{ "<Leader>sK", icon = { icon = nerdy.get("cod-split_vertical"), color = "blue" } },
 	-- Text
 	{ "<Leader>W",  icon = { icon = nerdy.get("cod-whitespace"), color = "grey" } },
+	-- Diagnostic
+	{ "[d",         icon = { icon = nerdy.get("md-stethoscope"), color = "orange" } },
+	{ "]d",         icon = { icon = nerdy.get("md-stethoscope"), color = "orange" } },
+	{ "[D",         icon = { icon = nerdy.get("md-stethoscope"), color = "orange" } },
+	{ "]D",         icon = { icon = nerdy.get("md-stethoscope"), color = "orange" } },
+	-- Quickfix
+	{ "[q",         icon = { icon = nerdy.get("cod-checklist"), color = "yellow" } },
+	{ "]q",         icon = { icon = nerdy.get("cod-checklist"), color = "yellow" } },
+	{ "[Q",         icon = { icon = nerdy.get("cod-checklist"), color = "yellow" } },
+	{ "]Q",         icon = { icon = nerdy.get("cod-checklist"), color = "yellow" } },
+	-- Location list
+	{ "[l",         icon = { icon = nerdy.get("md-format_list_bulleted"), color = "yellow" } },
+	{ "]l",         icon = { icon = nerdy.get("md-format_list_bulleted"), color = "yellow" } },
+	{ "[L",         icon = { icon = nerdy.get("md-format_list_bulleted"), color = "yellow" } },
+	{ "]L",         icon = { icon = nerdy.get("md-format_list_bulleted"), color = "yellow" } },
+	-- Buffers
+	{ "[b",         icon = { icon = nerdy.get("md-file_multiple"), color = "cyan" } },
+	{ "]b",         icon = { icon = nerdy.get("md-file_multiple"), color = "cyan" } },
+	{ "[B",         icon = { icon = nerdy.get("md-file_multiple"), color = "cyan" } },
+	{ "]B",         icon = { icon = nerdy.get("md-file_multiple"), color = "cyan" } },
+	-- Tabs
+	{ "[t",         icon = { icon = nerdy.get("md-tab"), color = "purple" } },
+	{ "]t",         icon = { icon = nerdy.get("md-tab"), color = "purple" } },
+	{ "[T",         icon = { icon = nerdy.get("md-tab"), color = "purple" } },
+	{ "]T",         icon = { icon = nerdy.get("md-tab"), color = "purple" } },
+	-- Shell prompts (OSC 133)
+	{ "[[",         icon = { icon = nerdy.get("md-console_line"), color = "green" } },
+	{ "]]",         icon = { icon = nerdy.get("md-console_line"), color = "green" } },
+	-- Blank lines
+	{ "[<Space>",   icon = { icon = nerdy.get("md-format_line_spacing"), color = "grey" } },
+	{ "]<Space>",   icon = { icon = nerdy.get("md-format_line_spacing"), color = "grey" } },
+	-- Methods
+	{ "[m",         icon = { icon = nerdy.get("md-function"), color = "blue" } },
+	{ "]m",         icon = { icon = nerdy.get("md-function"), color = "blue" } },
+	{ "[M",         icon = { icon = nerdy.get("md-function"), color = "blue" } },
+	{ "]M",         icon = { icon = nerdy.get("md-function"), color = "blue" } },
+	-- Spelling
+	{ "[s",         icon = { icon = nerdy.get("md-spellcheck"), color = "red" } },
+	{ "]s",         icon = { icon = nerdy.get("md-spellcheck"), color = "red" } },
+	-- Brackets
+	{ "[(",         icon = { icon = nerdy.get("md-code_parentheses"), color = "grey" } },
+	{ "](",         icon = { icon = nerdy.get("md-code_parentheses"), color = "grey" } },
+	{ "[{",         icon = { icon = nerdy.get("md-code_brackets"), color = "grey" } },
+	{ "]{",         icon = { icon = nerdy.get("md-code_brackets"), color = "grey" } },
+	{ "[%",         icon = { icon = nerdy.get("md-code_brackets"), color = "grey" } },
+	{ "]%",         icon = { icon = nerdy.get("md-code_brackets"), color = "grey" } },
 })
