9ccd0d08405f7c7fbe16a6c8b60a494330fa301a
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2026-05-16 12:14:53 +0200

    vim: Move config off `plugin.lsp.lua`

---
+++ vim/lua/lsp.lua
@@ -0,0 +1,53 @@
+local M = {}
+
+M.capabilities = require("blink.cmp").get_lsp_capabilities({
+	general = {
+		positionEncodings = { "utf-8" },
+	},
+})
+
+M.on_attach = function(_client, bufnr)
+	vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
+	local fzf_lua = require("fzf-lua")
+
+	local map = function(key, fn, desc)
+		vim.keymap.set("n", key, fn, { buffer = bufnr, silent = true, desc = desc })
+	end
+	map("gd", vim.lsp.buf.declaration, "Go to declaration")
+	map("gD", vim.lsp.buf.definition, "Go to definition")
+	map("K", function()
+		local diagnostics =
+				vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })
+		if #diagnostics > 0 then
+			vim.diagnostic.open_float()
+		else
+			vim.lsp.buf.hover()
+		end
+	end, "Diagnostic / hover")
+	map("gi", fzf_lua.lsp_implementations, "Go to implementation")
+	map("gr", fzf_lua.lsp_references, "References")
+	map("<Leader>lD", vim.lsp.buf.type_definition, "Type definition")
+	map("<Leader>lr", vim.lsp.buf.rename, "Rename")
+	map("<Leader>la", function()
+		fzf_lua.lsp_code_actions {
+			winopts = {
+				relative = "cursor",
+				width = 0.5,
+				height = 0.6,
+				row = 1, -- bottom
+				preview = { vertical = "up:65%" },
+			}
+		}
+	end, "Code action")
+	map("<Leader>lf", function()
+		vim.lsp.buf.format({ async = true })
+	end, "Format")
+	map("<Leader>lW", fzf_lua.lsp_workspace_diagnostics, "Workspace diagnostics")
+	map("<Leader>lwa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
+	map("<Leader>lwr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
+	map("<Leader>lwl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, "List workspace folders")
+end
+
+return M
--- vim/plugin/lsp.lua
+++ vim/plugin/lsp.lua
@@ -25,7 +25,7 @@ for _, lsp in ipairs(servers) do
 	vim.lsp.enable(lsp)
 end
 
--- LSP config
+-- Diagnostics
 
 vim.keymap.set("n", "<Leader>ld", vim.diagnostic.open_float, { silent = true, desc = "Show diagnostic" })
 vim.keymap.set("n", "[d", function()
@@ -36,49 +36,7 @@ vim.keymap.set("n", "]d", function()
 end, { silent = true, desc = "Next diagnostic" })
 vim.keymap.set("n", "<Leader>lq", vim.diagnostic.setloclist, { silent = true, desc = "Diagnostics to loclist" })
 
-local on_attach = function(_client, bufnr)
-	vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
-	local fzf_lua = require("fzf-lua")
-
-	local map = function(key, fn, desc)
-		vim.keymap.set("n", key, fn, { buffer = bufnr, silent = true, desc = desc })
-	end
-	map("gd", vim.lsp.buf.declaration, "Go to declaration")
-	map("gD", vim.lsp.buf.definition, "Go to definition")
-	map("K", function()
-		local diagnostics =
-				vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })
-		if #diagnostics > 0 then
-			vim.diagnostic.open_float()
-		else
-			vim.lsp.buf.hover()
-		end
-	end, "Diagnostic / hover")
-	map("gi", fzf_lua.lsp_implementations, "Go to implementation")
-	map("gr", fzf_lua.lsp_references, "References")
-	map("<Leader>lD", vim.lsp.buf.type_definition, "Type definition")
-	map("<Leader>lr", vim.lsp.buf.rename, "Rename")
-	map("<Leader>la", function()
-		fzf_lua.lsp_code_actions {
-			winopts = {
-				relative = "cursor",
-				width = 0.5,
-				height = 0.6,
-				row = 1, -- bottom
-				preview = { vertical = "up:65%" },
-			}
-		}
-	end, "Code action")
-	map("<Leader>lf", function()
-		vim.lsp.buf.format({ async = true })
-	end, "Format")
-	map("<Leader>lW", fzf_lua.lsp_workspace_diagnostics, "Workspace diagnostics")
-	map("<Leader>lwa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
-	map("<Leader>lwr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
-	map("<Leader>lwl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, "List workspace folders")
-end
+-- Config
 
 vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function()
@@ -87,11 +45,9 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	desc = "Format on write",
 })
 
+local lsp = require("lsp")
+
 vim.lsp.config("*", {
-	on_attach = on_attach,
-	capabilities = require("blink.cmp").get_lsp_capabilities({
-		general = {
-			positionEncodings = { "utf-8" },
-		},
-	}),
+	on_attach = lsp.on_attach,
+	capabilities = lsp.capabilities,
 })
