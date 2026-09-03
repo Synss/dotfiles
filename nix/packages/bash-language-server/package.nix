e6ba8228e605ce97b47381f06eff4f2982698d88
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2026-09-03 22:14:29 +0200

    nix: Wrap jj with watchman on PATH via makeWrapper
    watchman was a separate home.packages entry, paired to jj only by a
    comment. Wrapping jj's PATH to include it makes the pairing enforced
    by the package, not by convention.

--- nix/packages/bash-language-server/package.nix
+++ nix/packages/bash-language-server/package.nix
@@ -6,11 +6,11 @@
   shellcheck,
 }:
 symlinkJoin {
+  inherit bash-language-server;
   name = "bash-language-server-wrapped-${bash-language-server.version}";
   paths = [ bash-language-server ];
   nativeBuildInputs = [ makeWrapper ];
   postBuild = ''
     wrapProgram $out/bin/bash-language-server --prefix PATH : ${lib.makeBinPath [ shellcheck ]}
   '';
-  meta = bash-language-server.meta;
 }
--- nix/packages/nil/package.nix
+++ nix/packages/nil/package.nix
@@ -6,11 +6,11 @@
   nixfmt,
 }:
 symlinkJoin {
+  inherit nil;
   name = "nil-wrapped-${nil.version}";
   paths = [ nil ];
   nativeBuildInputs = [ makeWrapper ];
   postBuild = ''
     wrapProgram $out/bin/nil --prefix PATH : ${lib.makeBinPath [ nixfmt ]}
   '';
-  meta = nil.meta;
 }
