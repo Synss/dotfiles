da1b432603dd2f242dcb7b3c47f746d35ff3cb8f
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2026-09-07 14:39:26 +0200

    nix: Split terminal config into modules/terminal.nix

---
+++ nix/modules/terminal.nix
@@ -0,0 +1,62 @@
+{
+  pkgs,
+  config,
+  dotfilesDir,
+  ...
+}:
+let
+  inherit (import ./lib.nix { inherit config dotfilesDir; }) mkLink;
+in
+{
+  fonts.fontconfig.enable = true;
+
+  home.packages = with pkgs; [
+    nerd-fonts.hack
+    nerd-fonts.jetbrains-mono
+  ];
+
+  home.file.".config/dotfiles" = mkLink "shared";
+
+  programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        terminal.shell = {
+          program = "zsh";
+          args = [ "-l" ];
+        };
+        font = {
+          normal.family = "JetBrainsMono Nerd Font Mono";
+          size = 12.0;
+        };
+      };
+    };
+
+    bat = {
+      enable = true;
+      config.style = "header-filename,header-filesize,rule";
+    };
+
+    tmux = {
+      enable = true;
+      prefix = "C-Space";
+      mouse = true;
+      terminal = "tmux-256color";
+      keyMode = "vi";
+      escapeTime = 0;
+      extraConfig = ''
+        set -ga terminal-overrides ",*:Tc"
+        bind C-Space send-prefix
+      '';
+      plugins = with pkgs.tmuxPlugins; [
+        resurrect
+        {
+          plugin = continuum;
+          extraConfig = ''
+            set -g @continuum-restore 'on'
+          '';
+        }
+      ];
+    };
+  };
+}
