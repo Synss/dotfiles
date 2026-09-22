b2333e8309d11093b86a5309fee31ca76394b48c
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2026-09-22 17:31:33 +0200

    ci: Add bootstrapping test

--- nix/hosts.nix
+++ nix/hosts.nix
@@ -9,4 +9,14 @@
     stateVersion = "25.11";
     username = "laurin";
   };
+  "github-actions-macos" = {
+    system = "aarch64-darwin";
+    stateVersion = "25.11";
+    username = "runner";
+  };
+  "github-actions-linux" = {
+    system = "x86_64-linux";
+    stateVersion = "25.11";
+    username = "runner";
+  };
 }
