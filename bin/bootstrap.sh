a3f1eecf0a09e8a7678fe04c622696bcc70acfa4
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2026-09-23 15:32:15 +0200

    bootstrap.sh: Refactor + colocate jj

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,6 +2,29 @@
 
 set -euo pipefail
 
+main() {
+	local host
+	host=${1:-$(hostname -s)}
+
+	local restart=0
+
+	log info bootstrap_start platform="$(uname -s)"
+	install_nix
+	switch "$host"
+	colocate_jj
+	log info bootstrap_ok
+
+	if [ "$restart" -ne 0 ]; then
+		# shellcheck disable=SC2016
+		echo 'Bootstrapping done.  Restart your shell with "exec $SHELL -l".'
+	fi
+	exit "$restart"
+}
+
+request_restart() {
+	restart=1
+}
+
 log() {
 	# logfmt (github.com/kr/logfmt)
 	local level=$1
@@ -12,29 +35,30 @@ log() {
 		"$(date -u +%FT%TZ)" "$level" "$event" "${*:+ $*}"
 }
 
-main() {
+install_nix() {
 	command -v nix &>/dev/null && return 0
-
-	local host
-	host=${1:-$(hostname -s)}
-
-	log info bootstrap_start platform="$(uname -s)"
 
 	log info step_start step=install_nix
 	curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install ${CI:+--no-confirm}
+	request_restart
 	# shellcheck disable=SC1091
 	. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 	log info step_ok step=install_nix
+}
 
+switch() {
+	local host="$1"
 	log info step_start step=switch_home_manager
 	nix run home-manager -- switch --flake ".#${host}"
 	log info step_ok step=switch_home_manager
+}
 
-	log info bootstrap_ok
+colocate_jj() {
+	[ -d .jj ] && return 0
 
-	# shellcheck disable=SC2016
-	echo 'Bootstrapping done.  Restart your shell with "exec $SHELL -l".'
-	exit 1
+	log info step_start step=colocate_jj
+	git colocate
+	log info step_ok step=colocate_jj
 }
 
 main "$@"
